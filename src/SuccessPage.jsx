82f0a7130b0de6c11510fc7d53a1c546ceaa0017
hajarghalfi <hajarghalfi97@gmail.com>
2025-02-19 20:53:58 -0500

    this is for main

--- src/SuccessPage.jsx
+++ src/SuccessPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const SuccessPage = () => {
   return (
     <div>
-      <link>http://127.0.0.1:5501/jsx/my-app/src/second.html</link>
+      <link>http://127.0.0.1:5500/src/second.html</link>
       <p>Your form was submitted successfully.</p>
     </div>
   );
